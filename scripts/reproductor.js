b8e0505d79811e0d67c97d7ea3f57107a65f80f2
Cydonia8 <holymustaine20@gmail.com>
2023-05-12 19:27:34 +0200

    cuantización de colores para obtener los más relevantes

--- scripts/reproductor.js
+++ scripts/reproductor.js
@@ -286,6 +286,8 @@ async function initializeUser(){
     profile_menu_avatar.src=usuario_datos[0].foto_avatar
 }
 
+
+
 async function showAlbum(target){
     main_content.innerHTML=''
     loader.classList.remove("d-none")
@@ -298,8 +300,8 @@ async function showAlbum(target){
     const datos_album = datos["datos_album"]
     
     const section_album_head = document.createElement("section")
-    section_album_head.classList.add("container-fluid", "d-flex", "album-page-header", "gap-3", "align-items-center", "p-3", "border-bottom")
-    section_album_head.innerHTML=`<img src='${datos_album[0].foto}' class='album-page-portada'>
+    section_album_head.classList.add("container-fluid", "d-flex","flex-column", "flex-lg-row", "album-page-header", "gap-3", "align-items-center", "p-3", "border-bottom")
+    section_album_head.innerHTML=`<canvas></canvas>
                                     <div class='d-flex flex-column gap-3'>
                                         <h1>${datos_album[0].titulo}</h1>
                                         <div class='d-flex align-items-center gap-2'>
@@ -312,6 +314,25 @@ async function showAlbum(target){
                                             <i class="fa-regular fa-comment add-album-review"></i>
                                         </div>
                                     </div>`
+    const canvas = section_album_head.querySelector("canvas")
+    const img = document.createElement("img")
+    // canvas.style.width='300'
+    canvas.width='300'
+    // canvas.style.height='300'
+    canvas.height='300'
+    img.src=`${datos_album[0].foto}`
+    let ctxt = canvas.getContext("2d")
+    // const pattern = ctxt.createPattern(img, 'no-repeat');
+    // ctxt.fillStyle=pattern
+    // ctxt.fill()
+    ctxt.drawImage(img, 10, 10, 300, 300)
+    const image_data = ctxt.getImageData(0,0,canvas.width, canvas.height)
+    let rgb_array = buildRGBArray(image_data.data)
+    const quantColors = quantization(rgb_array, 0)
+    quantColors.sort((a,b) => a-b)
+    console.log(quantColors)
+    // buildPalette(quantColors);
+    
     main_content.appendChild(section_album_head)
     const lista_canciones = datos["lista_canciones"]
     const section_lista_canciones = document.createElement("section")
@@ -409,3 +430,95 @@ function playSong(index){
     player_logo.classList.add("active")
 
 }
+
+function buildRGBArray(imageData){
+const rgbValues = [];
+  for (let i = 0; i < imageData.length; i += 4) {
+    const rgb = {
+      r: imageData[i],
+      g: imageData[i + 1],
+      b: imageData[i + 2],
+    };
+    rgbValues.push(rgb);
+  }
+  return rgbValues;
+}
+
+function findBiggestColorRange(rgb_array){
+    let rMin = Number.MAX_VALUE;
+    let gMin = Number.MAX_VALUE;
+    let bMin = Number.MAX_VALUE;
+  
+    let rMax = Number.MIN_VALUE;
+    let gMax = Number.MIN_VALUE;
+    let bMax = Number.MIN_VALUE;
+  
+    rgb_array.forEach((pixel) => {
+      rMin = Math.min(rMin, pixel.r);
+      gMin = Math.min(gMin, pixel.g);
+      bMin = Math.min(bMin, pixel.b);
+  
+      rMax = Math.max(rMax, pixel.r);
+      gMax = Math.max(gMax, pixel.g);
+      bMax = Math.max(bMax, pixel.b);
+    });
+  
+    const rRange = rMax - rMin;
+    const gRange = gMax - gMin;
+    const bRange = bMax - bMin;
+  
+    const biggestRange = Math.max(rRange, gRange, bRange);
+    if (biggestRange === rRange) {
+      return "r";
+    } else if (biggestRange === gRange) {
+      return "g";
+    } else {
+      return "b";
+    }
+  };
+
+  function quantization(rgbValues, depth){
+    const MAX_DEPTH = 4;
+  
+    // Base case
+    if (depth === MAX_DEPTH || rgbValues.length === 0) {
+      const color = rgbValues.reduce(
+        (prev, curr) => {
+          prev.r += curr.r;
+          prev.g += curr.g;
+          prev.b += curr.b;
+  
+          return prev;
+        },
+        {
+          r: 0,
+          g: 0,
+          b: 0,
+        }
+      );
+  
+      color.r = Math.round(color.r / rgbValues.length);
+      color.g = Math.round(color.g / rgbValues.length);
+      color.b = Math.round(color.b / rgbValues.length);
+  
+      return [color];
+    }
+  
+    /**
+     *  Recursively do the following:
+     *  1. Find the pixel channel (red,green or blue) with biggest difference/range
+     *  2. Order by this channel
+     *  3. Divide in half the rgb colors list
+     *  4. Repeat process again, until desired depth or base case
+     */
+    const componentToSortBy = findBiggestColorRange(rgbValues);
+    rgbValues.sort((p1, p2) => {
+      return p1[componentToSortBy] - p2[componentToSortBy];
+    });
+  
+    const mid = rgbValues.length / 2;
+    return [
+      ...quantization(rgbValues.slice(0, mid), depth + 1),
+      ...quantization(rgbValues.slice(mid + 1), depth + 1),
+    ];
+  };
